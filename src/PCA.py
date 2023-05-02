beccbd2d36b0c1cc17795343211005bdb65348f1
Qin <118118177+threeefingers@users.noreply.github.com>
2023-05-01 22:34:28 -0400

    Update and rename feature_extraction.py to PCA.py

--- src/PCA.py
+++ src/PCA.py
@@ -1,5 +1,4 @@
 from sklearn.decomposition import TruncatedSVD
-from data_prepocessing import load_data,create_diag_m1
 import pandas as pd
 
 class PCAClassifier:
@@ -17,23 +16,6 @@ class PCAClassifier:
     def fit_transform(self, X, y=None):
         self.fit(X)
         return self.transform(X)
-
-def get_feature():
-    # Load data
-    data = load_data()
-
-    # Create diag_m1
-    diag_m1 = create_diag_m1()
-
-    # Create proc_code
-    # proc_code = create_proc_code()
-
-    # Perform PCA on diag_m1
-    pca = PCAClassifier(n_components=10)
-    features = pca.fit_transform(diag_m1)
-    sample_ids = diag_m1.index
-    df = pd.DataFrame(data=features, index=sample_ids)
-    return df
 
 
 if __name__ == '__main__':
