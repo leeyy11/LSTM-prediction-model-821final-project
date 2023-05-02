99d96131f66d6e789fc3b51441867e9fcfa584ec
Qin <118118177+threeefingers@users.noreply.github.com>
2023-05-02 04:36:01 -0400

    Final version!

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,16 +1,22 @@
 """Clinical data with ICD code preprocessing."""
 import pandas as pd
-from PCA import PCAClassifier
+from PCA import (
+    PCAClassifier,
+)
 import numpy as np
 
 
 class DataProcessor:
     """Define a dataprocessor."""
 
-    def __init__(self, diag_filename="DXCCSR.csv"):
+    def __init__(
+        self,
+        diag_filename="DXCCSR.csv",
+    ):
         """Define the ICD reference file."""
         self.reference = pd.read_csv(diag_filename)
-        self.reference = self.reference[
+        refer = self.reference
+        refer = refer[
             [
                 "'ICD-10-CM CODE'",
                 "'CCSR CATEGORY 1 DESCRIPTION'",
@@ -21,9 +27,13 @@ class DataProcessor:
                 "'CCSR CATEGORY 6 DESCRIPTION'",
             ]
         ]
-        self.reference = self.reference.replace("'", "", regex=True)
-        self.reference = pd.melt(
-            self.reference,
+        refer = refer.replace(
+            "'",
+            "",
+            regex=True,
+        )
+        refer = pd.melt(
+            refer,
             id_vars=["'ICD-10-CM CODE'"],
             value_vars=[
                 "'CCSR CATEGORY 1 DESCRIPTION'",
@@ -34,36 +44,73 @@ class DataProcessor:
                 "'CCSR CATEGORY 6 DESCRIPTION'",
             ],
         )
+        refer = refer[refer["value"] != " "]
+        refer = refer.drop(columns=["variable"])
+        self.reference = refer
 
     def diag_categorize(
-        self, n_component: int, diag_data: pd.DataFrame([[str]])
-    ) -> pd.DataFrame([[int]]):
-        """Catergorize and perform PCA diagnoses data."""
+        self,
+        diag_data: pd.DataFrame[[[str]]],
+    ) -> pd.DataFrame[[[int]]]:
+        """Catergorize diagnoses data."""
         # merge diagnoses data with ICD code reference
-        diag = diag_data[diag_data["icd_version"] == "10"][["subject_id", "icd_code"]]
+        diag = diag_data[diag_data["icd_version"] == "10"][
+            [
+                "subject_id",
+                "icd_code",
+            ]
+        ]
         diag = diag.merge(
-            self.reference, left_on="icd_code", right_on="'ICD-10-CM CODE'"
+            self.reference,
+            left_on="icd_code",
+            right_on="'ICD-10-CM CODE'",
         )
-        diag = diag.set_index("subject_id")
 
         # pivot the catergorized diagnoses data
         diag["count"] = 1
-        diag = diag[["value", "count"]]
+        diag = diag[
+            [
+                "subject_id",
+                "value",
+                "count",
+            ]
+        ]
         cat_diag = diag.pivot_table(
-            index="subject_id", columns="value", values="count", fill_value=0
+            index="subject_id",
+            columns="value",
+            values="count",
+            fill_value=0,
         )
-        cat_diag = cat_diag.drop(cat_diag.columns[0], axis=1)
+        # cat_diag = cat_diag.drop(cat_diag.columns[0], axis=1)
 
-        # perform PCA
-        pca = PCAClassifier(n_component)
+        return cat_diag
+
+    def diag_pca(
+        self,
+        n_components: int,
+        cat_diag: pd.DataFrame[[[int]]],
+    ) -> pd.DataFrame[[[int]]]:
+        """Perform PCA on data."""
+        pca = PCAClassifier(n_components)
         diag_features = pca.fit_transform(cat_diag.values)
-        data = pd.DataFrame(data=diag_features, index=cat_diag.index)
+        pca_diag = pd.DataFrame(
+            data=diag_features,
+            index=cat_diag.index,
+        )
 
-        return data
+        return pca_diag
 
-    def load_data(self, data_filename: str) -> pd.DataFrame([[int]]):
+    def data_load(
+        self,
+        n_components: int,
+        data_filename: str,
+    ) -> pd.DataFrame[[[int]]]:
         """Read in csv file of diagnoses data and reformat to dataframe."""
-        diag_data = pd.read_csv(data_filename)
-        data_final = self.diag_categorize(diag_data)
+        diag_data = pd.read_csv(data_filename).astype(str)
+        cat_diag = self.diag_categorize(diag_data)
+        data_final = self.diag_pca(
+            n_components,
+            cat_diag,
+        )
 
         return data_final
